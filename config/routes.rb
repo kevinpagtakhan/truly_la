c0fdbd91c0d86848cf58a76b3ee32f595ca1532d
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-09-19 11:18:32 -0700

    Show reviews on product page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   resources :users
-  resources :products
+  resources :products do
+    resources :reviews
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
